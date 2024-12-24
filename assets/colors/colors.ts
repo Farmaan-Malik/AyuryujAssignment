3467d588f3d2de273b965d3653edf41707e5fbf4
Farmaan-Malik <farmaanmalik285@gmail.com>
2024-12-24 21:11:25 +0530

    assets etc

---
+++ assets/colors/colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+    Blue: '#649BEC',
+    Green:'#85EFC3',
+    PrimaryGradient:['tomato','wheat'],
+    SecondaryGradient:['wheat','tomato'],
+    BorderedGradient:['#649BEC','#A2F8D9'],
+    GreenGradient:["#6DE4B3","#9AEFCC"],
+    PrimaryCardGradient:["#A698FF","#80E7DA"],
+    SecondaryCardGradient:["#71EEC1","#9AD0EF"]
+
+}
